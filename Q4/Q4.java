93ff5912a40e1e9e1f11572278e12878d08b59e1
Sean Egan <seaneganx@gmail.com>
2016-05-31 15:12:41 -0500

    added looping solution (instead of library call)

--- Q4/Q4.java
+++ Q4/Q4.java
@@ -17,6 +17,9 @@ public class Q4
 		int height;
 		String curLine;
 		int i; // for looping through stuff
+		int j; // reversing the string
+		char[] lineArray; // the current line represented as a char array so we can switch characters
+		char temp; // temporary storage for the character swap
 
 		// run tests from 1 to the given number of cases
 		for( testCase = 1; testCase <= numCases; testCase++ )
@@ -38,6 +41,29 @@ public class Q4
 				// reverse the current line
 				// java library calls can be convenient if you can find them during the competition
 				curLine = new StringBuilder(curLine).reverse().toString();
+
+				/* // using loops and a character array instead of a library call. Notice how difficult and time consuming this can be in the competition (find libraries if you can)
+				lineArray = curLine.toCharArray();
+
+				// dividing the size of the string by two always results in us stopping before the "middle" of the string (even/odd lengths have a different idea of "middle")
+				for( j = 0; j < lineArray.length / 2; j++ )
+				{
+					// save the left side character so we can swap it with its pair at the other end of the string
+					temp = lineArray[j];
+
+					// put the right side character into the left side spot
+					lineArray[j] = lineArray[ lineArray.length - j - 1 ];
+
+					// put the left side character we saved into the right side spot
+					lineArray[ lineArray.length - j - 1 ] = temp;
+				}
+
+				// replace the current line we read in with the array of the reversed characters
+				curLine = "";
+				for( j = 0; j < lineArray.length; j++ )
+				{
+					curLine += lineArray[j];
+				} */
 
 				// print out the current line
 				System.out.println( curLine );
